a095b3363b90a005ceff744362eef90bfe514d03
brunomffl <brunocardoso07@outlook.com>
2025-03-09 19:06:22 -0300

    Adicionando gitignore e conectando com o banco

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const db = require("./routes/db-config");
+const app = express();
+const cookie = require("cookie-parser");
+const cookieParser = require("cookie-parser");
+const PORT = process.env.PORT
+
+app.use("/js", express.static(__dirname + "./public/js"))
+app.use("/js", express.static(__dirname + "./public/css"))
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(cookie());
+app.use(express.json());
+db.connect((error) => {
+    if(error){
+        throw error;
+    }else{
+        console.log("banco de dados conectado")
+    }
+})
+app.listen(PORT)
