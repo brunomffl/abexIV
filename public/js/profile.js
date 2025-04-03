ddc2c1c5c127dfca395e0ba2d7f5ba1ed12dab0e
brunomffl <brunocardoso07@outlook.com>
2025-04-03 19:55:07 -0300

    Adicionando botão de editar conta

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,17 +1,24 @@
 const headerLinks = document.querySelectorAll("#header nav ul li a");
-const activeLink = document.querySelector("#header nav ul li a.active")
-console.log(headerLinks);
+const activeLink = document.querySelector("#header nav ul li a.active");
+const editButton = document.querySelector(".container .left .edit-icon");
+const containerLeft = document.querySelector(".container .left");
+const containerRight = document.querySelector(".container .right");
 
 headerLinks.forEach((link) => {
     link.addEventListener("mouseenter",() => {
         activeLink.classList.remove('active');
         link.classList.add('active');
     })
-})
+});
 
 headerLinks.forEach((link) => {
     link.addEventListener("mouseleave",() => {
         link.classList.remove('active');
         activeLink.classList.add('active');
     })
+});
+
+editButton.addEventListener("click", () => {
+    containerLeft.style.opacity = 0;
+    containerRight.style.opacity = 0;
 })
